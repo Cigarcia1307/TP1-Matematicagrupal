a1574a7693d8c57fa731990a7098b11b34326671
eugeniademarchi <demarchieugenia23@gmail.com>
2025-04-15 14:09:35 -0300

    Actualiza ejerciciogrupal.py con nuevas funciones

--- ejerciciogrupal.py
+++ ejerciciogrupal.py
@@ -1,28 +1,30 @@
-binario=""
+ingreso=(int(input('Para convertir de decimal a binario ingrese 1, de binario a decimal ingrese 0: ')))
+binario=''   
+    
+if ingreso == 1 :
+    numero=int(input("Ingrese el numero que desea pasar a binario:"))
+    binario=""
 
-numero=int(input("Ingrese un numero que desea pasar a binario:"))
+    while numero > 0:
+        if numero %2 == 0:
+            binario="0"+ binario
+        else:
+            binario="1"+ binario
+        numero=numero//2
 
+    print("El numero en binario es:", binario)
 
-while numero > 0:
-    if numero %2 == 0:
-        binario="0"+ binario
-    else:
-        binario="1"+ binario
-    numero=numero//2
-
-print("El numero en binario es:", binario)
-
+elif ingreso == 0:  
 #Pasar de binario a decimal
-
-decimal=0
-pos=len(binario)
-
-for i in reversed (binario):
-    if i == "1":
-        decimal=decimal + pow(2,(len(binario)-pos))
-    pos=pos-1
-
-print("el nuemro en decimal es:",decimal)
-
-
+    binario=input('ingresa el binario')
+    decimal=0
+    potencia=0
+    for digito in reversed(binario):
+      if digito == '1':
+          decimal += 2 ** potencia
+      potencia += 1
+    print(f'el binario ingresado: {binario}, es en decimal: {decimal}')
+ 
+else:
+    print('Ingreso invalido.')
 
