6b8355420d95eae4280c69f7769d7077255c60c2
fau05 <faustogagliano05@gmail.com>
2025-04-15 18:06:39 -0300

    Codigo con final

--- ejerciciogrupal.py
+++ ejerciciogrupal.py
@@ -1,30 +1,39 @@
-ingreso=(int(input('Para convertir de decimal a binario ingrese 1, de binario a decimal ingrese 0: ')))
-binario=''   
+while True:
     
-if ingreso == 1 :
-    numero=int(input("Ingrese el numero que desea pasar a binario:"))
-    binario=""
+    ingreso = int(input('\nPara convertir de decimal a binario ingrese 1, de binario a decimal ingrese 0: '))
 
-    while numero > 0:
-        if numero %2 == 0:
-            binario="0"+ binario
+    if ingreso == 1:
+        # Decimal a binario
+        numero = int(input("Ingrese el número que desea pasar a binario: "))
+        binario = ""
+
+        if numero == 0:
+                binario = "0"
         else:
-            binario="1"+ binario
-        numero=numero//2
+            while numero > 0:
+                binario = str(numero % 2) + binario
+                numero = numero // 2
 
-    print("El numero en binario es:", binario)
+        print("El número en binario es:", binario)
 
-elif ingreso == 0:  
-#Pasar de binario a decimal
-    binario=input('ingresa el binario')
-    decimal=0
-    potencia=0
-    for digito in reversed(binario):
-      if digito == '1':
-          decimal += 2 ** potencia
-      potencia += 1
-    print(f'el binario ingresado: {binario}, es en decimal: {decimal}')
- 
-else:
-    print('Ingreso invalido.')
+    elif ingreso == 0:
+        # Binario a decimal
+        binario = input('Ingresa el número binario: ')
+        if reversed(binario):
+            decimal = 0
+            potencia = 0
+            for digito in reversed(binario):
+                if digito == '1':
+                    decimal += 2 ** potencia
+                potencia += 1
+            print(f'El binario ingresado: {binario}, es en decimal: {decimal}')
+        else:
+            print("Error: el número ingresado no es binario válido (solo debe contener 0 y 1).")
 
+    else:
+        print('Ingreso inválido. Debes ingresar 0 o 1.')
+
+    repetir = input('\n¿Deseas realizar otra conversión? (si / no): ').lower()
+    if repetir != 'si':
+        print("¡Hasta la próxima!")
+        break
